9230db1f2657cd24d14acb7b733ee2fa999171a4
tornikeodo <61107611+tornikeodo@users.noreply.github.com>
2024-07-31 21:50:44 +0400

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-const { 
-  Client, 
-  GatewayIntentBits, 
-  REST, 
-  Routes, 
-  SlashCommandBuilder, 
-  ActivityType, 
-  ActionRowBuilder, 
-  ButtonBuilder, 
-  ButtonStyle, 
-  TextChannel, 
-  PermissionsBitField 
-} = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder, ActivityType, ActionRowBuilder, ButtonBuilder, ButtonStyle, TextChannel, PermissionsBitField } = require('discord.js');
 const noblox = require('noblox.js');
 require('dotenv').config();
 const express = require('express');
@@ -18,13 +6,13 @@ const fs = require('fs');
 const path = require('path');
 
 const client = new Client({ 
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageReactions] 
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages] 
 });
 
 const token = process.env.TOKEN;
 const clientId = '1261242922557247489';
 const guildId = '1260910227155325010';
-const robloxCookie = process.env.ROBLOX_COOKIE; // Use an environment variable for security
+const robloxCookie = '_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_0AC6CD4DBDEEDBF0461AACA59D4C2A14CA57144820FF4DFFB8E0B3D16CDF32FAA49EFC0274FAB00436D740F776553E3DF905E1B45F914097233D584623AD7C48CF5660171AA85142BD81328E0E02014AAA1E78E75E27572595552CDABC3B80F82B173C573FA39B1BB7E43AFB3263B7023A53951F90A3FCC1E350257F82C903A8460B6DA5B1F47069278A0645CA83292DF62A080B1A832341DE0CD3D33270DF3172781F3E0018AB73410820CF78BCA6E46C97E6C691E28CD0080CACCEBD7AC00A100B5A161C8D77EEF98BC4DA971F4EA9C0CA2C389EAA1D693A60AEAE788B21A1000E0DCB1EFFA1DF8D70393E53B3A091E21EA2A0FDF0275B59B2A1B0BE9DD2359F7D49C2DBB508D96AB7CCF4BC5F80B1FF30A45AE7081D540F087F48EBBBEADB1D1ACB22805ABC118155929EE2528D3BC1AC5E9F7C23517DC6F0C504FD31CFF2D52C3463A844D5EC75EFC7FC38D5EC956B260A2683529A8D099EC2885D5AE133F0E07E66713DF0FF208B0D9EC6287A3B6BB203A8FB8EE71D604EDD768CBA3528733DEFAD39A6C440665BDC9EA22A462301AED6DD78E0393951D0D3ACE5B8C7D40C6BEE9CFDFF5320540D2EF2FEFC19118FF72E61EC0BC3C31C9B6DF710E3DA847FBAAC0B02709BEFCB5B73452E590AF707A0E4B8676B5A93E36B157598120A854B5419054A767B310C81F74235672A9B8EA2D75BFF1AAB3AA55D68D37B6FAB6B47E03F9728CE1CBACCF400A8A418F5BF690F273B23BBFBFC2BCAC6F17B7A24D45E5F6436D471F4F92CFCC17DEA0DFB183FFFB93F393D5A437B40699B6BF3BDA0663F920F5FB424C640C8A0D730C51BF4C8A7ECCFADDD7F76932154D2949073B4D709F7D7896EBBA55CA6326E0FA19FD3F07CF7412BCC30FC22E081036953C5520D2A86627319FA45F823E9C79AF613EF62AEF0571DE0E8E19E0CF071B95F9AE14FD8E700BF240BDC8E46F4121E309C26AC5C6BBC80EF84332668D09C8D3F276F064856BF2E3767DA07AE30383E8E654AAF5DCAF08AB830E581F51163BFF71F0D411F9CA443F4989A397BAA45ACB124274D973867EA8386A16807DD49C4E30532E6307B04'; // Use an environment variable for security
 const groupId = 32304173; // PRL Professional Roblox League group ID
 
 const usedFriendlyCommand = new Set();
@@ -216,8 +204,8 @@ client.on('interactionCreate', async interaction => {
     const hoster = interaction.options.getString('hoster');
     const details = interaction.options.getString('details');
 
-    const specificRole = '1262465654317908060'; // Replace with the role ID
-    const pingRole = '1260910227155325018'; // Replace with the role ID
+    const specificRole = 'ROLE_ID'; // Replace with the role ID
+    const pingRole = 'ROLE_ID'; // Replace with the role ID
 
     if (!interaction.member.roles.cache.has(specificRole)) {
       await interaction.reply({ content: 'You do not have the required role to use this command.', ephemeral: true });
@@ -237,7 +225,7 @@ client.on('interactionCreate', async interaction => {
       },
     };
 
-    const channel = client.channels.cache.get('1260910228199968901'); // Replace with your specific channel ID
+    const channel = client.channels.cache.get('SPECIFIC_CHANNEL_ID'); // Replace with your specific channel ID
     if (channel) {
       const message = await channel.send({ content: `<@&${pingRole}>`, embeds: [embed] });
       await interaction.reply('Your scrim request has been submitted.');
